3bd3e3e9aff96f2ad519ae2c93dd8fa5b246207a
Abdalluh Mousa <abdallah.mousa@dexef.net>
2025-04-16 12:52:19 +0200

    add publish profile show swagger documentation in production environment

--- SallaStoreIntegration/Program.cs
+++ SallaStoreIntegration/Program.cs
@@ -32,11 +32,11 @@ builder.Services.Configure<ExternalStoresSetting>(builder.Configuration.GetSecti
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
